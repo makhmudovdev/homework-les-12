3ec9d6655c2f4d05e39561f7eea860089f003133
makhmudovdev <xursandmadiyarov2004@gmail.com>
2024-01-30 00:04:46 +0500

    Next store projekt

---
+++ lesson-15/index.js
@@ -0,0 +1,24 @@
+// let objekt = document.querySelector(".objekt");
+// objekt.style.width = "200px";
+// objekt.style.height = "200px";
+// objekt.style.backgroundColor = "blue";
+// objekt.style.color = "white";
+// objekt.style.alignItems = "center";
+// objekt.style.borderRadius = "20px"
+// objekt.style.border = "5px dashed black"
+
+// console.log("objekt");
+
+
+// const students = [
+//     {name: "Quincy", percent: 96 },
+//     { name: "Jason", percent: 84 },
+//     { name: "Alexis", percent: 100 },
+//     { name: "Sam", percent: 65 },  
+//     {name: "Katie", percent: 90 }, 
+//     {name: "Anna", percent: 75},
+//     ];
+
+// let arr = students.filter((el) => el.percent >= 65 && el.percent <= 84)
+
+// console.log(arr);
